2cc124d4f84e0bc0e91894141c64392f4be0c714
Maciej Turlo <maciej.turlo@gmail.com>
2016-09-29 02:06:26 +0200

    regexing the incoming message for triggers

--- src/main/scala/actors/LunchbotActor.scala
+++ src/main/scala/actors/LunchbotActor.scala
@@ -5,17 +5,30 @@ import com.cyberdolphins.slime.SlackBotActor.{Close, Connect}
 import com.cyberdolphins.slime.incoming._
 import com.cyberdolphins.slime.outgoing._
 import com.cyberdolphins.slime.common._
+import play.api.Logger
 
 /**
   * Created by mactur on 29/09/2016.
   */
 class LunchbotActor extends SlackBotActor {
 
+  val logger: Logger = Logger(getClass)
+
+  val triggers: Set[String] = Set("lunchbot", "lb")
+
+  val triggersRegex: String = triggers.map(tr => s"\\b$tr\\b").mkString("|")
+
+  logger.debug(s"triggersRegex is $triggersRegex")
+
+  val regex = s"($triggersRegex)(.*)"
+
+  logger.debug(s"regex is $regex")
+
   override def eventReceive: EventReceive = {
 
-    case SimpleInboundMessage(text, channel, user) =>
+    case SimpleInboundMessage(text, channel, user) if text.matches(regex) =>
 
-      publish(s"Echo: $text", channel, user)
+      publish(s"""Hello, ${user.name}, I can hear you say: *$text*""", channel, user)
 
   }
 
